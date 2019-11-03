d8a73521dd734764160e42bf10941ee85585dfb3
GaisinMD <goodzilla365@mail.com>
2019-11-03 20:45:25 +0300

    Верстка товары, форма. Джаваскрипт формы

---
+++ source/js/form.js
@@ -0,0 +1,63 @@
+'use strict';
+
+var ESC_KEYCODE = 27;
+var form = document.querySelector('.wrapper-form');
+var nameFocus = form.querySelector('input[name=name]');
+var formHeader = form.querySelector('h2');
+var formText = form.querySelector('p');
+var formHeaderContent = 'Остались вопросы? Задайте их нам!';
+var formTextContent = 'Мы проконсультируем Вас бесплатно';
+var formHeaderContentMobile = 'Закажите звонок';
+var formTextContentMobile = 'Оставьте контакты, мы проконсультируем вас бесплатно в удобное время';
+var closeFormButton = form.querySelector('.close-form');
+var popupClass = 'popup';
+var visuallyHiddenClass = 'visually-hidden';
+var mainButtonForForm = document.querySelector('.links-wrapper').querySelector('button');
+var secondButtonForForm = document.querySelector('.promo').querySelector('button');
+
+function setFormContent(header, text) {
+  if (formHeader) {
+    formHeader.textContent = header;
+  }
+  if (formText) {
+    formText.textContent = text;
+  }
+}
+
+function deactivateForm() {
+  form.classList.remove(popupClass);
+  nameFocus.blur();
+  closeFormButton.removeEventListener('click', deactivateForm);
+  document.removeEventListener('keydown', onEscPress);
+  closeFormButton.classList.add(visuallyHiddenClass);
+  setFormContent(formHeaderContent, formTextContent);
+}
+
+function onEscPress(evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    deactivateForm();
+  }
+};
+
+function hideForm() {
+  closeFormButton.addEventListener('click', deactivateForm);
+  document.addEventListener('keydown', onEscPress);
+}
+
+function activateForm() {
+  if (form) {
+    nameFocus.focus();
+    if (!form.classList.contains(popupClass)) {
+      form.classList.add(popupClass);
+      setFormContent(formHeaderContentMobile, formTextContentMobile);
+    }
+    if (closeFormButton || closeFormButton.classList.contains(visuallyHiddenClass)) {
+      closeFormButton.classList.remove(visuallyHiddenClass);
+    }
+    hideForm();
+  }
+}
+
+mainButtonForForm.addEventListener('click', activateForm);
+secondButtonForForm.addEventListener('click', activateForm);
+setFormContent(formHeaderContent, formTextContent);
--- source/js/script.js
+++ source/js/script.js
@@ -7,6 +7,8 @@ var mobileButtonText = 'Бесплатная консультация';
 var buttonText = 'Получить бесплатную консультацию';
 var mobileAboutText = 'Наши поставщики - мировые производители электронных компонентов: OSRAM, CREE, HOLGLITRONIC, REFOND. Печатные платы и комплектующие Service Devices применяются на предприятиях Российских Железных Дорог..';
 var aboutText = 'Наши поставщики - мировые производители электронных компонентов: OSRAM, CREE, HOLGLITRONIC, REFOND. Печатные платы и комплектующие Service Devices применяются на предприятиях Российских Железных Дорог (РЖД), РоссАвтоПрома (ВАЗ, АвтоГАЗ), МинАтома, СпецМедТехники. Среди наших клиентов крупнейшие Производители светотехники России.';
+var copyrightYear = document.querySelector('.copyright_year').cloneNode(true);
+var logoFooter = document.querySelector('.logo-footer');
 
 function changeElementText(element, width, text, replaceText) {
   if (element) {
@@ -18,10 +20,25 @@ function changeElementText(element, width, text, replaceText) {
   }
 }
 
+function replaceElement(nodeBefore, node, width) {
+  var copyrightYearNew = document.querySelector('.social').querySelector('.copyright_year')
+  if (node) {
+    if (document.body.clientWidth <= width) {
+      nodeBefore.after(node);
+    } else {
+      if (copyrightYearNew) {
+        copyrightYearNew.remove();
+      }
+    }
+  }
+}
+
 changeElementText(button, mobileMaxWidth, buttonText, mobileButtonText);
 changeElementText(textAbout, tabletMaxWidth, aboutText, mobileAboutText);
+replaceElement(logoFooter, copyrightYear, tabletMaxWidth);
 
 window.addEventListener('resize', function () {
   changeElementText(button, mobileMaxWidth, buttonText, mobileButtonText);
   changeElementText(textAbout, tabletMaxWidth, aboutText, mobileAboutText);
+  replaceElement(logoFooter, copyrightYear, tabletMaxWidth);
 });
