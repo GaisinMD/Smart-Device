13f2c1bc09272bed4a4066ecd7b4cac54c6ec85d
Marat <goodzilla365@mail.ru>
2019-11-04 23:58:58 +0300

    Стилизация form-popup завершена

--- source/js/form.js
+++ source/js/form.js
@@ -5,21 +5,27 @@ var form = document.querySelector('.wrapper-form');
 var nameFocus = form.querySelector('input[name=name]');
 var formHeader = form.querySelector('h2');
 var formText = form.querySelector('p');
+var formSubmit = form.querySelector('.submit-form');
 var formHeaderContent = 'Остались вопросы? Задайте их нам!';
 var formTextContent = 'Мы проконсультируем Вас бесплатно';
+var formSubmitContent = 'Задать вопрос';
 var formHeaderContentMobile = 'Закажите звонок';
 var formTextContentMobile = 'Оставьте контакты, мы проконсультируем вас бесплатно в удобное время';
+var formSubmitContentMobile = 'Отправить';
 var closeFormButton = form.querySelector('.close-form');
 var popupClass = 'popup';
 var visuallyHiddenClass = 'visually-hidden';
 var mainButtonForForm = document.querySelector('.links-wrapper').querySelector('button');
 
-function setFormContent(header, text) {
+function setFormContent(header, text, button) {
   if (formHeader) {
     formHeader.textContent = header;
   }
   if (formText) {
     formText.textContent = text;
+  }
+  if (formSubmit) {
+    formSubmit.textContent = button;
   }
 }
 
@@ -29,14 +35,14 @@ function deactivateForm() {
   closeFormButton.removeEventListener('click', deactivateForm);
   document.removeEventListener('keydown', onEscPress);
   closeFormButton.classList.add(visuallyHiddenClass);
-  setFormContent(formHeaderContent, formTextContent);
+  setFormContent(formHeaderContent, formTextContent, formSubmitContent);
 }
 
 function onEscPress(evt) {
   if (evt.keyCode === ESC_KEYCODE) {
     deactivateForm();
   }
-};
+}
 
 function hideForm() {
   closeFormButton.addEventListener('click', deactivateForm);
@@ -48,14 +54,15 @@ function activateForm() {
     nameFocus.focus();
     if (!form.classList.contains(popupClass)) {
       form.classList.add(popupClass);
-      setFormContent(formHeaderContentMobile, formTextContentMobile);
+      setFormContent(formHeaderContentMobile, formTextContentMobile, formSubmitContentMobile);
     }
     if (closeFormButton || closeFormButton.classList.contains(visuallyHiddenClass)) {
       closeFormButton.classList.remove(visuallyHiddenClass);
     }
+    window.scrollTo(pageXOffset, 0);
     hideForm();
   }
 }
 
 mainButtonForForm.addEventListener('click', activateForm);
-setFormContent(formHeaderContent, formTextContent);
+setFormContent(formHeaderContent, formTextContent, formSubmitContent);
