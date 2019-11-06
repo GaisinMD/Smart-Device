d4fae2a06af8d156357255620e1bad5e4909ea53
Marat <goodzilla365@mail.ru>
2019-11-06 20:37:04 +0300

    Ревью наставника Общее - исправлено лого футера, исправлен баг js в футере

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,9 @@
 'use strict';
-var mobileMaxWidth = 767;
-var tabletMaxWidth = 1023;
+
+var hideClass = 'visually-hidden';
+
+var mobileMaxWidth = 768;
+var tabletMaxWidth = 1024;
 var textAbout = document.querySelector('.about').querySelectorAll('p')[1];
 var button = document.querySelector('.promo').querySelector('button');
 var mobileButtonText = 'Бесплатная консультация';
@@ -10,9 +13,38 @@ var aboutText = 'Наши поставщики - мировые производ
 var copyrightYear = document.querySelector('.copyright_year').cloneNode(true);
 var logoFooter = document.querySelector('.logo-footer');
 
+var buttonSections = document.querySelector('#sections');
+var buttonAddress = document.querySelector('#address');
+
+var buttonBackgroundOpen = 'url("/img/button_section_open.png")';
+var buttonBackgroundClose = 'url("/img/button_section_close.png")';
+
+var siteSections = document.querySelector('.site-sections').querySelector('ul');
+var address = document.querySelector('.address').querySelector('div');
+
+function toggleSection(section, buttonClose) {
+  if (section) {
+    if (section.classList.contains(hideClass)) {
+      section.classList.remove(hideClass);
+      buttonClose.style.backgroundImage = buttonBackgroundOpen;
+    } else {
+      section.classList.add(hideClass);
+      buttonClose.style.backgroundImage = buttonBackgroundClose;
+    }
+  }
+}
+
+function hideElement() {
+  if (document.body.clientWidth <= mobileMaxWidth) {
+    siteSections.classList.add(hideClass);
+  } else {
+    siteSections.classList.remove(hideClass);
+  }
+}
+
 function changeElementText(element, width, text, replaceText) {
   if (element) {
-    if (document.body.clientWidth > width) {
+    if (document.body.clientWidth >= width) {
       element.textContent = text;
     } else {
       element.textContent = replaceText;
@@ -21,7 +53,7 @@ function changeElementText(element, width, text, replaceText) {
 }
 
 function replaceElement(nodeBefore, node, width) {
-  var copyrightYearNew = document.querySelector('.social').querySelector('.copyright_year')
+  var copyrightYearNew = document.querySelector('.social').querySelector('.copyright_year');
   if (node) {
     if (document.body.clientWidth <= width) {
       nodeBefore.after(node);
@@ -33,12 +65,25 @@ function replaceElement(nodeBefore, node, width) {
   }
 }
 
+buttonSections.style.backgroundImage = buttonBackgroundClose;
+hideElement();
 changeElementText(button, mobileMaxWidth, buttonText, mobileButtonText);
 changeElementText(textAbout, tabletMaxWidth, aboutText, mobileAboutText);
 replaceElement(logoFooter, copyrightYear, tabletMaxWidth);
+
+buttonSections.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  toggleSection(siteSections, evt.target);
+});
+
+buttonAddress.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  toggleSection(address, evt.target);
+});
 
 window.addEventListener('resize', function () {
   changeElementText(button, mobileMaxWidth, buttonText, mobileButtonText);
   changeElementText(textAbout, tabletMaxWidth, aboutText, mobileAboutText);
   replaceElement(logoFooter, copyrightYear, tabletMaxWidth);
+  hideElement();
 });
