d61e6732837fc6c93fc538ddcda9e7260fcb3332
Marat <goodzilla365@mail.ru>
2019-11-13 15:22:08 +0300

    Ревью наставника - убран JS код с перемещением

--- build/js/main.js
+++ build/js/main.js
@@ -1,15 +1,62 @@
 'use strict';
 
+var ESC_KEYCODE = 27;
 var hideClass = 'visually-hidden';
+
+// переменные формы
+var body = document.querySelector('body');
+var popupForm = document.querySelector('.popup');
+var nameFocus = popupForm.querySelector('input[name=name]');
+var closeFormButton = popupForm.querySelector('.feedback-form__close');
+var mainButtonForForm = document.querySelector('.header__button');
+var scrollDenaiedStyle = 'position: fixed; overflow: hidden;';
+var scrollNormalStyle = 'position: static; overflow: auto;';
+
+// переменные аккардеона
 var mobileMaxWidth = '(max-width: 767px)';
-
 var toggleSecitons = document.querySelectorAll('.toggle-section');
-
 var classPictureClose = '.toggle-section__toggle-close';
 var classPictureOpen = '.toggle-section__toggle-open';
 var classSectionText = '.toggle-section__text';
+var classButton = '.toggle-section__toggle';
 
+// функции формы
+function deactivateForm() {
+  popupForm.classList.add(hideClass);
+  nameFocus.blur();
+  body.style = scrollNormalStyle;
+  closeFormButton.removeEventListener('click', deactivateForm);
+  document.removeEventListener('keydown', onEscPress);
+}
 
+function onEscPress(evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    deactivateForm();
+  }
+}
+
+function hideForm() {
+  popupForm.addEventListener('click', function (evt) {
+    if (evt.target === closeFormButton || evt.target === popupForm) {
+      deactivateForm();
+    }
+  });
+  document.addEventListener('keydown', onEscPress);
+}
+
+function activateForm() {
+  if (popupForm) {
+    nameFocus.focus();
+    if (popupForm.classList.contains(hideClass)) {
+      popupForm.classList.remove(hideClass);
+    }
+    window.scrollTo(pageXOffset, 0);
+    body.style = scrollDenaiedStyle;
+    hideForm();
+  }
+}
+
+// функции аккардеона
 function hideSection(section, openPic, closePic) {
   if (section) {
     section.classList.add(hideClass);
@@ -69,9 +116,8 @@ function initToogles() {
     var openIcon = elem.querySelector(classPictureOpen);
     var closeIcon = elem.querySelector(classPictureClose);
     var site = elem.querySelector(classSectionText);
+    var button = elem.querySelector(classButton);
 
-
-    var button = elem.querySelector('.toggle-section__toggle');
     if (window.matchMedia(mobileMaxWidth).matches) {
       hideSection(site, openIcon, closeIcon);
       button.addEventListener('click', setEvent);
@@ -85,3 +131,5 @@ function initToogles() {
 initToogles();
 
 window.addEventListener('resize', initToogles);
+
+mainButtonForForm.addEventListener('click', activateForm);
