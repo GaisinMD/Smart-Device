03a9a1c7648b45d619db53e5d0e92cafe603d0ba
Marat <goodzilla365@mail.ru>
2019-11-13 15:52:42 +0300

    Ревью наставниеа и баглист - закончено

--- source/js/main.js
+++ source/js/main.js
@@ -20,6 +20,11 @@ var classPictureOpen = '.toggle-section__toggle-open';
 var classSectionText = '.toggle-section__text';
 var classButton = '.toggle-section__toggle';
 
+// переменные скролла
+var anchors = [].slice.call(document.querySelectorAll('a[href*="#"]'));
+var animationTime = 300;
+var framesCount = 20;
+
 // функции формы
 function deactivateForm() {
   popupForm.classList.add(hideClass);
@@ -127,6 +132,27 @@ function initToogles() {
     }
   }
 }
+
+// реализация скролла
+anchors.forEach(function (item) {
+  item.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    var coordY = document.querySelector(item.getAttribute('href')).getBoundingClientRect().top + window.pageYOffset;
+
+    var scroller = setInterval(function () {
+      var scrollBy = coordY / framesCount;
+
+      if (scrollBy > window.pageYOffset - coordY && window.innerHeight + window.pageYOffset < document.body.offsetHeight) {
+        window.scrollBy(0, scrollBy);
+      } else {
+        window.scrollTo(0, coordY);
+        clearInterval(scroller);
+      }
+    }, animationTime / framesCount);
+  });
+});
+
 
 initToogles();
 
